c2d877b04c2b7ed7624c29f66ac0466a855eb613
kiran-muddana <66030578+kiran-muddana@users.noreply.github.com>
2024-04-21 14:32:21 -0400

    Delete survey0.1 directory

--- survey0.1/src/main/java/com/hw/survey01/repository/SurveyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hw.survey01.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hw.survey01.model.Survey;
-
-public interface SurveyRepository extends JpaRepository<Survey, Long>{
-
-}
